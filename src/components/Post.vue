a562875d0aefd228006c9176d3c4e28302f97f69
SAYATHAM <n.sayatham@gmail.com>
2022-12-08 12:07:37 +0100

    mise a jour du code complet

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,237 +1,42 @@
 <template>
-    <NavUser />
-    <h1 class="titre_post">Publication</h1>
+    <img :src="path" class="box_img" />
 
-    <form class="form_upload" action="upload-photo" @submit.prevent>
-        <h2>Selectionné une photo</h2>
+    <p class="post-title">{{ title }}</p>
+    <p class="post-desc">{{ description }}</p>
 
-        <input
-            class="file-input"
-            type="file"
-            @change="file"
-            accept=".png, .jpg, .jpeg"
-        />
-        <button class="btn_upload" @click="upload" formaction="upload-photo">
-            Validez votre photo
-        </button>
-        <h3 v-if="uploadSucces">{{ uploadSucces }}</h3>
-    </form>
-
-    <form class="form_post" action="post-publication" @submit.prevent>
-        <input type="text" v-model="title" placeholder="Titre" />
-        <!-- <p class="errors" v-if="this.errors.title" v-text="errors.title[0]"></p> -->
-
-        <textarea
-            type="text"
-            v-model="description"
-            placeholder="Description"
-            rows="4"
-            cols="50"
-        />
-        <!-- <p
-            class="errors"
-            v-if="this.errors.description"
-            v-text="errors.description[0]"
-        ></p> -->
-
-        <button class="btn_post" @click="post" formaction="post-publication">
-            Publié !
-        </button>
-    </form>
+    <!-- <img class="box-img" :src="`http://localhost:8000/storage/${elem.image}`" />
+    <p class="post-title">{{ elem.title }}</p>
+    <p class="post-desc">{{ elem.description }}</p> -->
 </template>
 
 <script>
-const token = localStorage.getItem("token");
-
-import axios from "axios";
-import NavUser from "./NavUser.vue";
 export default {
     name: "Post",
-    components: { NavUser },
     data() {
-        return {
-            title: "",
-            description: "",
-            image: "",
-            urlFile: "",
-            info: "",
-            urlImage: "",
-            uploadSucces: "",
-            errors: "",
-        };
+        return {};
     },
-    methods: {
-        file(e) {
-            this.image = e.target.files[0];
-            console.log(this.image);
-        },
-        async upload() {
-            const formData = new FormData();
-
-            formData.set("image", this.image);
-            await axios
-                .post("http://127.0.0.1:8000/api/upload", formData)
-                .then((response) => (this.urlImage = response.data));
-            console.log(this.urlImage.url);
-            this.uploadSucces = this.urlImage.message;
-            this.urlFile = this.urlImage.url;
-        },
-        async post() {
-            await axios({
-                method: "post",
-                headers: { Authorization: `Bearer ${token}` },
-                url: "http://127.0.0.1:8000/api/post",
-
-                data: {
-                    title: this.title,
-                    description: this.description,
-                    image: this.urlFile,
-                    id_user: 1,
-                },
-            })
-                .then((response) => (this.info = response.data))
-                .catch((error) => {
-                    this.errors = error.response.data.errors;
-                });
-            console.log(this.errors);
-
-            // location = "http://localhost:8080/post-thread";
-        },
-    },
+    props: ["path", "title", "description"],
+    methods: {},
 };
 </script>
 
 <style>
-@media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
-    .titre_post {
-        text-align: center;
-    }
-    .form_upload {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .form_post {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: left;
-    }
-
-    .btn_upload {
-        position: relative;
-        font-size: 14px;
-        text-transform: uppercase;
-        text-decoration: none;
-        padding: 0.5em 0.8em;
-        display: inline-block;
-        border-radius: 6em;
-        transition: all 0.2s;
-        border: none;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-            Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-        font-weight: 500;
-        color: white;
-        background-color: #5db7de;
-        margin: 10px;
-    }
-
-    .btn_upload:hover {
-        transform: translateY(-3px);
-        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
-    }
-
-    .btn_upload:active {
-        transform: translateY(-1px);
-        box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
-    }
-
-    .btn_upload::after {
-        content: "";
-        display: inline-block;
-        height: 100%;
-        width: 100%;
-        border-radius: 100px;
-        position: absolute;
-        top: 0;
-        left: 0;
-        z-index: -1;
-        transition: all 0.4s;
-    }
-
-    .btn_upload::after {
-        background-color: #fff;
-    }
-
-    .btn_upload:hover::after {
-        transform: scaleX(1.4) scaleY(1.6);
-        opacity: 0;
-    }
-
-    .form_post input,
-    textarea {
-        margin: 15px;
-        color: #5db7de;
-    }
-    .form_post label {
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-            Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-    }
-
-    .form_post input {
-        height: 25px;
-    }
-
-    .btn_post {
-        position: relative;
-        font-size: 17px;
-        text-transform: uppercase;
-        text-decoration: none;
-        padding: 1em 1.5em;
-        display: inline-block;
-        border-radius: 6em;
-        transition: all 0.2s;
-        border: none;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-            Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-        font-weight: 500;
-        color: white;
-        background-color: #5db7de;
-        margin: 10px;
-    }
-
-    .btn_post:hover {
-        transform: translateY(-3px);
-        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
-    }
-
-    .btn_post:active {
-        transform: translateY(-1px);
-        box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
-    }
-
-    .btn_post::after {
-        content: "";
-        display: inline-block;
-        height: 100%;
-        width: 100%;
-        border-radius: 100px;
-        position: absolute;
-        top: 0;
-        left: 0;
-        z-index: -1;
-        transition: all 0.4s;
-    }
-
-    .btn_post::after {
-        background-color: #fff;
-    }
-
-    .btn_post:hover::after {
-        transform: scaleX(1.4) scaleY(1.6);
-        opacity: 0;
-    }
+.box_img {
+    width: 300px;
+    height: auto;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+.post-title {
+    background: #bad7dd;
+    height: 50px;
+    margin: 0;
+    font-weight: bolder;
+}
+.post-desc {
+    background: #bad7dd;
+    height: auto;
+    margin: 0;
 }
 </style>
--- src/views/PostThreadView.vue
+++ src/views/PostThreadView.vue
@@ -1,12 +1,108 @@
 <template>
-    <PostThread></PostThread>
+    <NavUser />
+    <div class="wrapper-thread">
+        <h1>Fil d'actualitée</h1>
+        <button @click="forceUpdate">update</button>
+        <div class="box_post" v-for="elem in infos" :key="elem.id">
+            <Post
+                :path="`http://localhost:8000/storage/${elem.image}`"
+                :title="`${elem.title}`"
+                :description="`${elem.description}`"
+                :key="postReload"
+            ></Post>
+            <div class="box_btn">
+                <div class="box_btn_like">
+                    <ButtonLike
+                        @click="likePost(elem.id, elem.id_user)"
+                        :like="`${elem.like}`"
+                    ></ButtonLike>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import PostThread from "@/components/PostThread.vue";
+const token = localStorage.getItem("token");
+// const id_post = localStorage.getItem("id_post");
+
+import axios from "axios";
+
+import NavUser from "../components/NavUser.vue";
+
+import ButtonLike from "@/components/ButtonLike.vue";
+import Post from "@/components/Post.vue";
+
 export default {
     name: "PostThreadView",
+    components: { NavUser, ButtonLike, Post },
+    data() {
+        return {
+            post: "",
+            infos: "",
+            dislike: "",
+            image: "",
+            infoLikes: "",
+            infoDisLikes: "",
+            like: "",
+            postReload: 0,
+        };
+    },
 
-    components: { PostThread },
+    async mounted() {
+        await axios({
+            method: "get",
+            headers: { Authorization: `Bearer ${token}` },
+            url: "http://127.0.0.1:8000/api/all-post",
+        }).then((response) => (this.infos = response.data.allPost));
+        console.log(this.infos);
+
+        this.image = this.infos.image;
+    },
+
+    methods: {
+        async likePost(id_post, id_user) {
+            axios({
+                method: "post",
+                headers: { Authorization: `Bearer ${token}` },
+                url:
+                    "http://127.0.0.1:8000/api/like/" + id_post + "/" + id_user,
+                data: {
+                    idPost: id_post,
+                },
+            }).then((response) => (this.like = response));
+
+            localStorage.setItem("id_post", id_post);
+
+            // relance de la requête pour afficher le nombre de like
+            await axios({
+                method: "get",
+                headers: { Authorization: `Bearer ${token}` },
+                url: "http://127.0.0.1:8000/api/all-post",
+            }).then((response) => (this.infos = response.data.allPost));
+            console.log(this.infos);
+        },
+    },
 };
 </script>
+
+<style>
+@media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+    /* CSS */
+
+    .btn_like {
+        font-size: 17px;
+    }
+    .box_btn {
+        display: flex;
+        justify-content: center;
+
+        height: auto;
+    }
+
+    .box_post {
+        border-bottom: #000000 solid 1px;
+        margin: 0;
+    }
+}
+</style>
